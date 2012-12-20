4a1f3cc2ee0ef8acb27ee8a793fbc6f487ba7858
Gregor MacDougall <gregor@freerunningtechnologies.com>
2012-12-20 09:59:47 -0800

    Initial commit
    Change-Id: Idef3f0c7feb173e6dbb42b5f0e202f7c4de96381

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reference::Application.config.secret_token = '04022f9a55be6f4ac7b2265c32afbe639b673f8bba89885ce2e2baaa537510ec234eeeec0f91ec8171420b9323f35ced0b59a4b784b1f86add8b7476fbb64828'
